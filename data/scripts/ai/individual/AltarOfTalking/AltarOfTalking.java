5939a2cdab8f242aed5ed85f80665dc8a0a2222b
Inia <Inia68@outlook.fr>
2018-05-05 01:23:13 +0200

    Fixed  type rename

--- data/scripts/ai/individual/AltarOfTalking/AltarOfTalking.java
+++ data/scripts/ai/individual/AltarOfTalking/AltarOfTalking.java
@@ -2,9 +2,9 @@ package ai.individual.AltarOfTalking;
 
 import l2server.gameserver.Announcements;
 import l2server.gameserver.ai.CtrlIntention;
-import l2server.gameserver.model.actor.Attackable;
-import l2server.gameserver.model.actor.Npc;
-import l2server.gameserver.model.actor.instance.Player;
+import l2server.gameserver.model.actor.L2Attackable;
+import l2server.gameserver.model.actor.L2Npc;
+import l2server.gameserver.model.actor.instance.L2PcInstance;
 import l2server.gameserver.model.quest.Quest;
 
 import java.util.HashMap;
@@ -20,16 +20,15 @@ public class AltarOfTalking extends Quest {
     private static Map<Integer, Boolean> spawnInfo = new HashMap<Integer, Boolean>(3);
     private static final int[] raidIds = {80248, 80247, 80246};
     private static final int[] stoneIds = {20770, 20771, 20772};
-    private static final int altarOfTalkingId = 80245;
 
     public AltarOfTalking(int questId, String name, String descr) {
         super(questId, name, descr);
 
         addSpawn(margueriteId, -119771, 246318, -1237, 843, false, 0);
 
-        addFirstTalkId(altarOfTalkingId);
-        addStartNpc(altarOfTalkingId);
-        addTalkId(altarOfTalkingId);
+        addFirstTalkId(margueriteId);
+        addStartNpc(margueriteId);
+        addTalkId(margueriteId);
 
         for (int i : raidIds) {
             addKillId(i);
@@ -38,12 +37,12 @@ public class AltarOfTalking extends Quest {
     }
 
     @Override
-    public String onFirstTalk(Npc npc, Player player) {
+    public String onFirstTalk(L2Npc npc, L2PcInstance player) {
         return "AltarOfTalking.html";
     }
 
     @Override
-    public String onAdvEvent(String event, Npc npc, Player player) {
+    public String onAdvEvent(String event, L2Npc npc, L2PcInstance player) {
         if (event.startsWith("trySpawnBoss")) {
             int bossId = Integer.valueOf(event.split(" ")[1]);
             int stoneId = 0;
@@ -68,7 +67,7 @@ public class AltarOfTalking extends Quest {
                     Announcements.getInstance().announceToAll("Marguerite: Mooooooh!");
                     spawnInfo.put(bossId, true); //Boss is spawned
 
-                    Attackable boss = (Attackable) addSpawn(bossId, npc.getX(), npc.getY() + 200, npc.getZ(), 0, false, 0, true);
+                    L2Attackable boss = (L2Attackable) addSpawn(bossId, npc.getX(), npc.getY() + 200, npc.getZ(), 0, false, 0, true);
                     boss.setIsRunning(true);
                     boss.setTarget(player);
                     boss.addDamageHate(player, 9999, 9999);
@@ -81,7 +80,7 @@ public class AltarOfTalking extends Quest {
     }
 
     @Override
-    public String onKill(Npc npc, Player player, boolean isPet) {
+    public String onKill(L2Npc npc, L2PcInstance player, boolean isPet) {
         synchronized (spawnInfo) {
             spawnInfo.put(npc.getNpcId(), false);
         }
