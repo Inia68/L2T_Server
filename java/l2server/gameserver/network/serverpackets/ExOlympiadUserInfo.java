4a05872966ffacf12b7f7e31d985abe2aefaa0f1
Sylight <sylight9@gmail.com>
2018-05-08 10:13:13 -0500

    Stupid config file

--- java/l2server/gameserver/network/serverpackets/ExOlympiadUserInfo.java
+++ java/l2server/gameserver/network/serverpackets/ExOlympiadUserInfo.java
@@ -17,7 +17,6 @@ package l2server.gameserver.network.serverpackets;
 
 import l2server.gameserver.model.actor.instance.L2PcInstance;
 import l2server.gameserver.model.olympiad.OlympiadParticipant;
-import org.omg.PortableServer.IMPLICIT_ACTIVATION_POLICY_ID;
 
 /**
  * This class ...
